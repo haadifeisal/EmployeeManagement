205f690a65892530642ed6173c05322495d60947
Haadi Feisal <haadii_94@hotmail.com>
2021-11-12 17:22:39 +0100

    EmployeeService UnitTest
    Unit Test for GetEmployees()

---
+++ EmployeeManagement.WebApi.Test/UnitTests/EmployeeServiceUnitTest.cs
@@ -0,0 +1,59 @@
+﻿using EmployeeManagement.WebApi.Repositories.EmployeeManagement;
+using EmployeeManagement.WebApi.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using FakeItEasy;
+using EmployeeManagement.WebApi.Services.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.WebApi.Test.UnitTests
+{
+    [TestClass]
+    public class EmployeeServiceUnitTest : UnitTestBase
+    {
+        [TestMethod]
+        public async Task Verify_GetEmployees_ReturnsACollectionOf2EmployeeObjects()
+        {
+            //Arrange
+            var department = new Department
+            {
+                DepartmentId = Guid.NewGuid(),
+                Name = "IT"
+            };
+            _employeeManagementcontext.Departments.Add(department);
+
+            var employee1 = new Employee
+            {
+                EmployeeId = Guid.NewGuid(),
+                Name = "Hanna",
+                Salary = 55000,
+                DepartmentId = department.DepartmentId
+            };
+            _employeeManagementcontext.Employees.Add(employee1);
+
+            var employee2 = new Employee
+            {
+                EmployeeId = Guid.NewGuid(),
+                Name = "Adam",
+                Salary = 47000,
+                DepartmentId = department.DepartmentId
+            };
+            _employeeManagementcontext.Employees.Add(employee2);
+            _employeeManagementcontext.SaveChanges();
+
+            var departmentServiceMock = A.Fake<IDepartmentService>();
+
+            A.CallTo(() => departmentServiceMock.GetDetpartments()).Returns(new List<Department>());
+
+            var employeeService = new EmployeeService(_mapper, _employeeManagementcontext, departmentServiceMock);
+
+            //Act
+            var employees = await employeeService.GetEmployees();
+
+            //Assert
+            Assert.AreEqual(2, employees.Count());
+        }
+    }
+}
---
+++ EmployeeManagement.WebApi.Test/UnitTests/UnitTestBase.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using EmployeeManagement.WebApi.DataTransferObjects.Configuration;
+using EmployeeManagement.WebApi.Repositories.EmployeeManagement;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.WebApi.Test.UnitTests
+{
+    public class UnitTestBase
+    {
+        public readonly EmployeeManagementContext _employeeManagementcontext;
+        public readonly IMapper _mapper;
+
+        public UnitTestBase()
+        {
+
+            var dbName = Guid.NewGuid().ToString();
+            var options = new DbContextOptionsBuilder<EmployeeManagementContext>()
+            .UseInMemoryDatabase(databaseName: dbName)
+            .Options;
+
+            _employeeManagementcontext = new EmployeeManagementContext(options);
+
+            var mappingConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new MapConfiguration());
+            });
+            IMapper mapper = mappingConfig.CreateMapper();
+
+            _mapper = mapper;
+
+        }
+    }
+}
